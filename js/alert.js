cf1a340685f1c2ad9d9a653e796d1686da655122
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2021-02-11 00:00:06 -0300

    adjusted pages of project

--- js/alert.js
+++ js/alert.js
@@ -1,6 +1,6 @@
 
 $( document ).ready(function() { 
     $('.contact-button').click(function(){
-        window.alert('Serviço indisponivel!!')
+        window.alert('Serviço indisponível!!')
     });
 });
